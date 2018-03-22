afdafd0dece2f0b3f73de14685031bc4e30072f2
Kolafim <719513675@qq.com>
2018-03-22 19:39:16 +0800

    dist

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-const Home = { template: '<div><h2>Home Page</h2></div>'}
-const About = { template: '<div><h2>About Page</h2></div>'}
-const Contact = { template: '<div><h2>Contact Page</h2></div>'}
+let Home = {template:'<div><h2>Home Page</h2></div>'}
+let About = {template:'<div><h2>About Page</h2></div>'}
+let Contact = {template:'<div><h2>Contact Page</h2></div>'}
 
 export default new Router({
   mode: 'history',
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/contact', component: Contact }
