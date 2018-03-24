cb77587e63eb55c77c5ba9c67f1d3f9a19e817d5
Kolafim <719513675@qq.com>
2018-03-24 17:03:17 +0800

    dist 15

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Home Page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,50 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+// import MetaInfo from 'vue-meta-info'
+import Home from '@/components/home'
+import About from '@/components/about'
+import Contact from '@/components/contact'
 
 Vue.use(Router)
-
-let Home = {template:'<div><h2>Home Page</h2></div>'}
-let About = {template:'<div><h2>About Page</h2></div>'}
-let Contact = {template:'<div><h2>Contact Page</h2></div>'}
+// Vue.use(MetaInfo)
 
 console.log(process.env);
 
-export default new Router({
+const BASE_TITLE = document.title;
+
+// let Home = {template:'<div><h2>Home Page</h2></div>'}
+// let About = {template:'<div><h2>About Page</h2></div>'}
+// let Contact = {template:'<div><h2>Contact Page</h2></div>'}
+
+const router = new Router({
   base: process.env.NODE_BASE_URL || '/',
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
-    { path: '/about', component: About, meta:{title: "About Page"}},
-    { path: '/contact', component: Contact, meta:{title: "Contact Page"}}
+    {
+      path: '/',
+      name:'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name:'about',
+      component: About,
+      meta:{title:'About'}
+    },
+    {
+      path: '/contact',
+      name:'contact',
+      component: Contact,
+      meta:{title:'Contact'}
+    }
   ]
 })
+router.beforeEach((to,from,next)=>{
+  if(to.meta.title){
+    document.title = `${to.meta.title} - ${BASE_TITLE}`
+  }else if(BASE_TITLE != document.title){
+    document.title = BASE_TITLE
+  }
+  next()
+})
+export default router
